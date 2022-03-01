80c7aafc02d92531b20a0a8b78d106106e45289c
bsayre <bsayre@iastate.edu>
2022-03-01 15:41:53 -0600

    yo

--- Chapter-7/photogallery/app.py
+++ Chapter-7/photogallery/app.py
@@ -195,4 +195,4 @@ def search_page():
             photos=items, searchquery=query)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5000)
